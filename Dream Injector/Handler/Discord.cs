856a3f4c75e423734e09b58b33a317ab1873b858
Woke <66000596+woke999@users.noreply.github.com>
2021-07-29 20:16:09 -0400

    Add files via upload

---
+++ Dream Injector/Handler/Discord.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Dream_Injector.Handler.Structs;
+using DiscordRPC;
+
+namespace Dream_Injector.Handler
+{
+    public static class Discord
+    {
+        public static DiscordRpcClient client;
+
+        public static void Start()
+        {
+            client = new DiscordRpcClient("870442249287245864");
+            client.Initialize();
+            client.SetPresence(new RichPresence()
+            {
+                Details = $"Dream Injector",
+                State = $"Idle",
+                Assets = new Assets()
+                {
+                    LargeImageKey = "logo",
+                    LargeImageText = "Dream DLL Injector",
+                }
+            });
+        }
+
+        public static void Update()
+        {
+            if (Idle)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = $"Dream Injector",
+                    State = $"Idle",
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "logo",
+                        LargeImageText = "Dream DLL Injector",
+                    }
+                });
+            }
+            else if (Injected)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = $"Dream Injector",
+                    State = $"Injected DLL",
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "logo",
+                        LargeImageText = "Dream DLL Injector",
+                    }
+                });
+            }
+            else if (DLLInjecting)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = $"Dream Injector",
+                    State = $"Injecting DLL",
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "logo",
+                        LargeImageText = "Dream DLL Injector",
+                    }
+                });
+            }
+            else if (SelectingDLL)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = $"Dream Injector",
+                    State = $"Selecting DLL",
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "logo",
+                        LargeImageText = "Dream DLL Injector",
+                    }
+                });
+            }
+            else if (SelectedDLL)
+            {
+                client.SetPresence(new RichPresence()
+                {
+                    Details = $"Dream Injector",
+                    State = $"Selected DLL",
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "logo",
+                        LargeImageText = "Dream DLL Injector",
+                    }
+                });
+            }
+        }
+    }
+}
---
+++ Dream Injector/Handler/Structs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dream_Injector.Handler
+{
+    public static class Structs
+    {
+        public static bool Idle;
+        public static bool Injected;
+        public static bool SelectingDLL;
+        public static bool SelectedDLL;
+        public static bool DLLInjecting;
+
+        public static void Idling()
+        {
+            Idle = true;
+        }
+
+        public static void Inject()
+        {
+            Injected = true;
+        }
+
+        public static void Selecting()
+        {
+            SelectingDLL = true;
+        }
+
+        public static void Selected()
+        {
+            SelectedDLL = true;
+        }
+
+        public static void Injecting()
+        {
+            DLLInjecting = true;
+        }
+
+        public static void Reset()
+        {
+            Idle = false;
+            DLLInjecting = false;
+            SelectingDLL = false;
+        }
+
+        public static void ResetAll()
+        {
+            Idle = false;
+            Injected = false;
+            SelectingDLL = false;
+            SelectedDLL = false;
+            DLLInjecting = false;
+        }
+    }
+}
