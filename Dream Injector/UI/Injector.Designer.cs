2391201177bcc0965258534fb3cdcf247a22f89a
Woke <66000596+woke999@users.noreply.github.com>
2021-07-29 20:21:15 -0400

    Add files via upload

---
+++ Dream Injector/UI/Injector.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace Dream_Injector
+{
+    partial class Injector
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.exitBtn = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.siticoneControlBox1 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.pathTextBox = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chooseBtn = new Siticone.UI.WinForms.SiticoneButton();
+            this.commonDropDown = new Siticone.UI.WinForms.SiticoneComboBox();
+            this.injectBtn = new Siticone.UI.WinForms.SiticoneButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.refreshBtn = new Siticone.UI.WinForms.SiticoneButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dream Injector";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Text", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(156, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User:";
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.exitBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.exitBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.exitBtn.HoveredState.Parent = this.exitBtn;
+            this.exitBtn.IconColor = System.Drawing.Color.White;
+            this.exitBtn.Location = new System.Drawing.Point(672, 0);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.PressedColor = System.Drawing.Color.White;
+            this.exitBtn.ShadowDecoration.Parent = this.exitBtn;
+            this.exitBtn.Size = new System.Drawing.Size(45, 27);
+            this.exitBtn.TabIndex = 5;
+            // 
+            // siticoneControlBox1
+            // 
+            this.siticoneControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.siticoneControlBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.siticoneControlBox1.ControlBoxType = Siticone.UI.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.siticoneControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.siticoneControlBox1.HoveredState.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox1.Location = new System.Drawing.Point(621, 0);
+            this.siticoneControlBox1.Name = "siticoneControlBox1";
+            this.siticoneControlBox1.PressedColor = System.Drawing.Color.White;
+            this.siticoneControlBox1.ShadowDecoration.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.Size = new System.Drawing.Size(45, 27);
+            this.siticoneControlBox1.TabIndex = 6;
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this;
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Animated = false;
+            this.pathTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.pathTextBox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.pathTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.pathTextBox.DefaultText = "";
+            this.pathTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.pathTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.pathTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.pathTextBox.DisabledState.Parent = this.pathTextBox;
+            this.pathTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.pathTextBox.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.pathTextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.pathTextBox.FocusedState.Parent = this.pathTextBox;
+            this.pathTextBox.Font = new System.Drawing.Font("Sitka Text", 12F);
+            this.pathTextBox.ForeColor = System.Drawing.Color.White;
+            this.pathTextBox.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.pathTextBox.HoveredState.Parent = this.pathTextBox;
+            this.pathTextBox.Location = new System.Drawing.Point(6, 78);
+            this.pathTextBox.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.PasswordChar = '\0';
+            this.pathTextBox.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(31)))), ((int)(((byte)(31)))));
+            this.pathTextBox.PlaceholderText = "";
+            this.pathTextBox.SelectedText = "";
+            this.pathTextBox.ShadowDecoration.Parent = this.pathTextBox;
+            this.pathTextBox.Size = new System.Drawing.Size(283, 37);
+            this.pathTextBox.TabIndex = 8;
+            this.pathTextBox.TextChanged += new System.EventHandler(this.pathTextBox_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Info;
+            this.label4.Location = new System.Drawing.Point(1, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 28);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "DLL";
+            // 
+            // chooseBtn
+            // 
+            this.chooseBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.chooseBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.chooseBtn.BorderRadius = 4;
+            this.chooseBtn.BorderThickness = 2;
+            this.chooseBtn.CheckedState.Parent = this.chooseBtn;
+            this.chooseBtn.CustomImages.Parent = this.chooseBtn;
+            this.chooseBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.chooseBtn.Font = new System.Drawing.Font("Sitka Text", 11.25F);
+            this.chooseBtn.ForeColor = System.Drawing.Color.White;
+            this.chooseBtn.HoveredState.Parent = this.chooseBtn;
+            this.chooseBtn.Location = new System.Drawing.Point(297, 78);
+            this.chooseBtn.Name = "chooseBtn";
+            this.chooseBtn.ShadowDecoration.Parent = this.chooseBtn;
+            this.chooseBtn.Size = new System.Drawing.Size(111, 37);
+            this.chooseBtn.TabIndex = 14;
+            this.chooseBtn.Text = "Choose DLL";
+            this.chooseBtn.Click += new System.EventHandler(this.chooseBtn_Click);
+            // 
+            // commonDropDown
+            // 
+            this.commonDropDown.Animated = true;
+            this.commonDropDown.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.commonDropDown.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.commonDropDown.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.commonDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.commonDropDown.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.commonDropDown.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.commonDropDown.Font = new System.Drawing.Font("Sitka Text", 14.25F);
+            this.commonDropDown.ForeColor = System.Drawing.Color.White;
+            this.commonDropDown.HoveredState.Parent = this.commonDropDown;
+            this.commonDropDown.ItemHeight = 30;
+            this.commonDropDown.ItemsAppearance.Parent = this.commonDropDown;
+            this.commonDropDown.Location = new System.Drawing.Point(6, 156);
+            this.commonDropDown.Name = "commonDropDown";
+            this.commonDropDown.ShadowDecoration.Parent = this.commonDropDown;
+            this.commonDropDown.Size = new System.Drawing.Size(283, 36);
+            this.commonDropDown.TabIndex = 17;
+            this.commonDropDown.SelectedIndexChanged += new System.EventHandler(this.commonDropDown_SelectedIndexChanged);
+            // 
+            // injectBtn
+            // 
+            this.injectBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.injectBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.injectBtn.BorderRadius = 4;
+            this.injectBtn.BorderThickness = 2;
+            this.injectBtn.CheckedState.Parent = this.injectBtn;
+            this.injectBtn.CustomImages.Parent = this.injectBtn;
+            this.injectBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.injectBtn.Font = new System.Drawing.Font("Sitka Text", 11.25F);
+            this.injectBtn.ForeColor = System.Drawing.Color.White;
+            this.injectBtn.HoveredState.Parent = this.injectBtn;
+            this.injectBtn.Location = new System.Drawing.Point(414, 156);
+            this.injectBtn.Name = "injectBtn";
+            this.injectBtn.ShadowDecoration.Parent = this.injectBtn;
+            this.injectBtn.Size = new System.Drawing.Size(111, 37);
+            this.injectBtn.TabIndex = 18;
+            this.injectBtn.Text = "Inject";
+            this.injectBtn.Click += new System.EventHandler(this.injectBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Text", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(1, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 28);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Process";
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.refreshBtn.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.refreshBtn.BorderRadius = 4;
+            this.refreshBtn.BorderThickness = 2;
+            this.refreshBtn.CheckedState.Parent = this.refreshBtn;
+            this.refreshBtn.CustomImages.Parent = this.refreshBtn;
+            this.refreshBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.refreshBtn.Font = new System.Drawing.Font("Sitka Text", 11.25F);
+            this.refreshBtn.ForeColor = System.Drawing.Color.White;
+            this.refreshBtn.HoveredState.Parent = this.refreshBtn;
+            this.refreshBtn.Location = new System.Drawing.Point(297, 155);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.ShadowDecoration.Parent = this.refreshBtn;
+            this.refreshBtn.Size = new System.Drawing.Size(111, 37);
+            this.refreshBtn.TabIndex = 20;
+            this.refreshBtn.Text = "Refresh";
+            this.refreshBtn.Click += new System.EventHandler(this.refreshBtn_Click);
+            // 
+            // Injector
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(717, 418);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.injectBtn);
+            this.Controls.Add(this.commonDropDown);
+            this.Controls.Add(this.chooseBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.siticoneControlBox1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Injector";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dream Injector";
+            this.Load += new System.EventHandler(this.Injector_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Siticone.UI.WinForms.SiticoneControlBox exitBtn;
+        private Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox1;
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+        private Siticone.UI.WinForms.SiticoneTextBox pathTextBox;
+        private System.Windows.Forms.Label label4;
+        public Siticone.UI.WinForms.SiticoneButton chooseBtn;
+        public Siticone.UI.WinForms.SiticoneButton injectBtn;
+        private Siticone.UI.WinForms.SiticoneComboBox commonDropDown;
+        private System.Windows.Forms.Label label3;
+        public Siticone.UI.WinForms.SiticoneButton refreshBtn;
+    }
+}
+
---
+++ Dream Injector/UI/Injector.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dream_Injector.Handler;
+
+namespace Dream_Injector
+{
+    public partial class Injector : Form
+    {
+        private static string DLLP { get; set; }
+        static readonly IntPtr INTPTR_ZERO = (IntPtr)0;
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInheritHandle, uint dwProcessId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int CloseHandle(IntPtr hObject);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, IntPtr dwSize, uint flAllocationType, uint flProtect);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] buffer, uint size, int lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        public Injector()
+        {
+            InitializeComponent();
+        }
+
+        private void Injector_Load(object sender, EventArgs e)
+        {
+            Discord.Start();
+            GetProcesses();
+        }
+
+        private void pathTextBox_TextChanged(object sender, EventArgs e)
+        {
+            DLLP = pathTextBox.Text;
+        }
+
+
+        private void chooseBtn_Click(object sender, EventArgs e)
+        {
+            Structs.ResetAll();
+            Structs.Selecting();
+            Discord.Update();
+            ChooseDLL();
+        }
+
+        private void refreshBtn_Click(object sender, EventArgs e)
+        {
+            GetProcesses();
+        }
+
+        private void GetProcesses()
+        {
+            Process[] PC = Process.GetProcesses().Where(p => (long)p.MainWindowHandle != 0).ToArray();
+            commonDropDown.Items.Clear();
+            foreach (Process p in PC)
+            {
+                commonDropDown.Items.Add(p.ProcessName);
+            }
+        }
+
+        private void ChooseDLL()
+        {
+            try
+            {
+                OpenFileDialog OFD = new OpenFileDialog();
+                OFD.InitialDirectory = @"C:/";
+                OFD.Title = "Select DLL";
+                OFD.DefaultExt = "dll";
+                //OFD.Filter = "DLL (*.dll) | *.dll)";
+                OFD.Filter = "DLL (*.dll)|*.dll";
+                OFD.CheckFileExists = true;
+                OFD.CheckPathExists = true;
+                OFD.ShowDialog();
+                DLLP = OFD.FileName;
+                pathTextBox.Text = OFD.FileName;
+                Structs.ResetAll();
+                Structs.Selected();
+                Discord.Update();
+            } 
+            catch (Exception ed)
+            {
+                MessageBox.Show("Dream Injector ran into an error, screenshot the next messagebox and show a developer to fix it");
+                MessageBox.Show(ed.Message);
+            }
+        }
+
+        public static int Inject(string PN, string DLLP)
+        {
+            //1 = file does not exist
+            //2 = Process not active
+            //3 = injection failed
+            //4 = injection succeeded
+
+            if (!File.Exists(DLLP)) { return 1; }
+
+            uint _procId = 0;
+            Process[] _procs = Process.GetProcesses();
+            for (int i = 0; i < _procs.Length; i++)
+            {
+                if (_procs[i].ProcessName == PN)
+                {
+                    _procId = (uint)_procs[i].Id;
+                }
+            }
+
+            if (_procId == 0) { return 2; }
+
+            if (!SI(_procId, DLLP))
+            {
+                return 3;
+            }
+
+            return 4;
+        }
+
+        public static bool SI(uint P, string DDLP)
+        {
+            IntPtr hndProc = OpenProcess((0x2 | 0x8 | 0x10 | 0x20 | 0x400), 1, P);
+
+            if (hndProc == INTPTR_ZERO) { return false; }
+
+
+            IntPtr lpAddress = VirtualAllocEx(hndProc, (IntPtr)null, (IntPtr)DLLP.Length, (0x1000 | 0x2000), 0x40);
+
+            if (lpAddress == INTPTR_ZERO)
+            {
+                return false;
+            }
+
+            byte[] bytes = Encoding.ASCII.GetBytes(DLLP);
+
+            if (WriteProcessMemory(hndProc, lpAddress, bytes, (uint)bytes.Length, 0) == 0)
+            {
+                return false;
+            }
+
+            CloseHandle(hndProc);
+
+            return true;
+        }
+
+        private void injectBtn_Click(object sender, EventArgs e)
+        {
+            Structs.ResetAll();
+            Structs.Injecting();
+            Discord.Update();
+            int Result = Inject(commonDropDown.Text, DLLP);
+            if (Result == 1)
+            {
+                MessageBox.Show("DLL does not exist", Application.ProductName);
+            }
+            else if (Result == 2)
+            {
+               MessageBox.Show("Process not found", Application.ProductName);
+
+            }
+            else if (Result == 3)
+            {
+                MessageBox.Show("Injection failed", Application.ProductName);
+            }
+            else if (Result == 4)
+            {
+                MessageBox.Show("Injection succeeded", Application.ProductName);
+                Structs.ResetAll();
+                Structs.Inject();
+                Discord.Update();
+
+            }
+        }
+
+        private void StartVacBypass()
+        {
+            try
+            {
+                Process.Start(string.Format("file:///{0}VAC-Bypass-Loader.exe", AppDomain.CurrentDomain.BaseDirectory));
+            }
+            catch (Exception ed)
+            {
+                MessageBox.Show("Dream Injector ran into an error starting vac bypasser, screenshot the next messagebox and show a developer");
+                MessageBox.Show(ed.Message);
+            }
+        }
+
+        private void commonDropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
